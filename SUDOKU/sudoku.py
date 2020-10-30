71ca9d480f512facdc4262f73a831638bc990e7a
SerGe <serge.rybakov@gmail.com>
2020-10-30 18:44:10 +0400

    Sudoku refactored

--- SUDOKU/sudoku.py
+++ SUDOKU/sudoku.py
@@ -1,3 +1,30 @@
+from pprint import pprint
+
+grid_false = [
+    [1, 2, 3, 4, 5, 6, 7, 8, 9],
+    [2, 3, 4, 5, 6, 7, 8, 9, 1],
+    [3, 4, 5, 6, 7, 8, 9, 1, 2],
+    [4, 5, 6, 7, 8, 9, 1, 2, 3],
+    [5, 6, 7, 8, 9, 1, 2, 3, 4],
+    [6, 7, 8, 9, 1, 2, 3, 4, 5],
+    [7, 8, 9, 1, 2, 3, 4, 5, 6],
+    [8, 9, 1, 2, 3, 4, 5, 6, 7],
+    [9, 1, 2, 3, 4, 5, 6, 7, 8]
+]
+
+grid_true = [
+    [1, 2, 3, 4, 5, 6, 7, 8, 9],
+    [4, 5, 6, 7, 8, 9, 1, 2, 3],
+    [7, 8, 9, 1, 2, 3, 4, 5, 6],
+    [2, 3, 4, 5, 6, 7, 8, 9, 1],
+    [5, 6, 7, 8, 9, 1, 2, 3, 4],
+    [8, 9, 1, 2, 3, 4, 5, 6, 7],
+    [9, 1, 2, 3, 4, 5, 6, 7, 8],
+    [3, 4, 5, 6, 7, 8, 9, 1, 2],
+    [6, 7, 8, 9, 1, 2, 3, 4, 5]
+]
+
+
 def check_line(line):
     for i in line:
         if line.count(i) > 1:
@@ -5,20 +32,33 @@ def check_line(line):
     return True
 
 
-def check_column(column):
-    for i in column:
-        if column.count(i) > 1:
+def check_sudoku(grid):
+    # выделяем строки
+    for line in grid:
+        if not check_line(line):
             return False
+
+    # выделяем колонки
+    for i in range(len(grid)):
+        column = [line[i] for line in grid]
+        if not check_line(column):
+            return False
+
+    # выделяем квадраты (3х3 по всей сетке с соответствующим шагом)
+    square = []
+    for line in grid[:3]:
+        print(line)
+        square.extend(line[:3])
+    print()
+    pprint(square)
+    if not check_line(square):
+        return False
+
     return True
 
 
-def check_square(line, column):
+print(check_sudoku(grid_true))
+print()
+print(check_sudoku(grid_false))
 
-    for j in line[::3]:
-        if line[::3][0] != column[::3][0]:
-            return False
-        if line[::3].count(line.index(j)) > 1:
-            return False
-        if column[::3].count(column.index(j)) > 1:
-            return False
-    return True
+
--- SUDOKU/sudoku_tests.py
+++ SUDOKU/sudoku_tests.py
@@ -1,29 +1,56 @@
 from random import randint
-from SUDOKU import sudoku
+
 import pytest
+
+from SUDOKU import sudoku
+
+grid_false = [
+    [1, 2, 3, 4, 5, 6, 7, 8, 9],
+    [2, 3, 4, 5, 6, 7, 8, 9, 1],
+    [3, 4, 5, 6, 7, 8, 9, 1, 2],
+    [4, 5, 6, 7, 8, 9, 1, 2, 3],
+    [5, 6, 7, 8, 9, 1, 2, 3, 4],
+    [6, 7, 8, 9, 1, 2, 3, 4, 5],
+    [7, 8, 9, 1, 2, 3, 4, 5, 6],
+    [8, 9, 1, 2, 3, 4, 5, 6, 7],
+    [9, 1, 2, 3, 4, 5, 6, 7, 8]
+]
+
+grid_true = [
+    [1, 2, 3, 4, 5, 6, 7, 8, 9],
+    [4, 5, 6, 7, 8, 9, 1, 2, 3],
+    [7, 8, 9, 1, 2, 3, 4, 5, 6],
+    [2, 3, 4, 5, 6, 7, 8, 9, 1],
+    [5, 6, 7, 8, 9, 1, 2, 3, 4],
+    [8, 9, 1, 2, 3, 4, 5, 6, 7],
+    [9, 1, 2, 3, 4, 5, 6, 7, 8],
+    [3, 4, 5, 6, 7, 8, 9, 1, 2],
+    [6, 7, 8, 9, 1, 2, 3, 4, 5]
+]
 
 
 @pytest.mark.parametrize('sample, expected', [
-    ([i for i in range(1, 10)], True),
-    ([randint(1, 9) for i in range(1, 10)], False),
+    (grid_true, True),
+    (grid_false, False),
 ])
 def test_check_line(sample, expected):
     assert expected == sudoku.check_line(sample)
 
 
-@pytest.mark.parametrize('sample, expected', [
-    ([i for i in range(1, 10)], True),
-    ([randint(1, 9) for i in range(1, 10)], False)
-])
-def test_check_column(sample, expected):
-    assert expected == sudoku.check_column(sample)
-
-@pytest.mark.parametrize('line, column, expected', [
-    ([i for i in range(1, 10)], [i for i in range(1, 10)], True),
-    ([randint(1, 9) for i in range(1, 10)], [randint(1, 9) for i in range(1, 10)], False)
-])
-def test_check_square(line, column, expected):
-    assert expected == sudoku.check_square(line, column)
+# @pytest.mark.parametrize('sample, expected', [
+#     ([i for i in range(1, 10)], True),
+#     ([randint(1, 9) for i in range(1, 10)], False)
+# ])
+# def test_check_column(sample, expected):
+#     assert expected == sudoku.check_column(sample)
+#
+#
+# @pytest.mark.parametrize('line, column, expected', [
+#     ([i for i in range(1, 10)], [i for i in range(1, 10)], True),
+#     ([randint(1, 9) for i in range(1, 10)], [randint(1, 9) for i in range(1, 10)], False)
+# ])
+# def test_check_square(line, column, expected):
+#     assert expected == sudoku.check_square(line, column)
 
 
 if __name__ == '__main__':
