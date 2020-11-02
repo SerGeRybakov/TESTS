e14cb4aaa7bf5e39ce1e4928b7eb559fd2933833
SerGe <serge.rybakov@gmail.com>
2020-11-02 18:16:51 +0400

    Sorted lists code

---
+++ SORTED_LISTS/sorted_lists.py
@@ -0,0 +1,65 @@
+"""Есть функция, на вход которой поступает произвольное количество отсортированных списков.
+Функция должна слить эти списки в один отсортированный список.
+Использовать sort или sorted само собой нельзя."""
+from random import randint
+
+
+def get_min_element(args):
+    return min(min(args))
+
+
+def create_new_sorted_list(args, min_element):
+    new_lst = []
+    for lst in args:
+        if min_element in lst:
+            new_lst.extend(lst)
+            args.remove(lst)
+            return new_lst, args
+
+
+def insert_in_place(new_list, new_element):
+    for element in new_list:
+        if element < new_element:
+            continue
+        new_list.insert(new_list.index(element), new_element)
+        return new_list
+
+
+def _check_nums(new_lst, next_list):
+    for new_element in next_list:
+        if new_element > new_lst[-1]:
+            return new_lst.extend(next_list[next_list.index(new_element)::])
+
+        if new_element in new_lst:
+            new_lst.insert(new_lst.index(new_element), new_element)
+        else:
+            insert_in_place(new_lst, new_element)
+    return new_lst
+
+
+def merge_lists(args, new_lst):
+    for lst in args:
+        _check_nums(new_lst, lst)
+    return new_lst
+
+
+def main():
+    a = sorted([randint(x, 10) for x in range(10)])
+    a1 = sorted([randint(x, 10) for x in range(10)])
+    b = sorted([randint(x, 50) for x in range(50)])
+    b1 = sorted([randint(x, 50) for x in range(50)])
+    c = sorted([randint(x, 100) for x in range(100)])
+    c1 = sorted([randint(x, 100) for x in range(100)])
+    d = sorted([randint(x, 1000) for x in range(1000)])
+    d1 = sorted([randint(x, 1000) for x in range(1000)])
+
+    args = [a, a1, b, b1, c, c1, d, d1]
+
+    min_element = get_min_element(args)
+    new_list, args = create_new_sorted_list(args, min_element)
+
+    return merge_lists(args, new_list)
+
+
+if __name__ == '__main__':
+    print(main())
---
+++ SORTED_LISTS/test_sorted_lists.py
@@ -0,0 +1,12 @@
+import pytest
+
+from SORTED_LISTS import sorted_lists
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    pytest.main()
