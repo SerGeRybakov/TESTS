4be0f52d506da2b067f255f9a9c4ac78010eebc7
SerGe <serge.rybakov@gmail.com>
2020-11-08 12:41:45 +0400

    Rectangles refactored

--- RECTANGLES_INTERSECTION/rectangles.py
+++ RECTANGLES_INTERSECTION/rectangles.py
@@ -1,4 +1,6 @@
 class Rectangle:
+    __slots__ = ("x1", "y1", "x2", "y2", "rect")
+
     def __init__(self, x1, y1, x2, y2):
         self.x1 = x1
         self.y1 = y1
@@ -8,35 +10,41 @@ class Rectangle:
         self.rect = [[self.x1, self.y1], [self.x2, self.y2]]
 
     def __str__(self):
-        return f'[[{self.x1}, {self.y1}], [{self.x2}, {self.y2}]]'
+        return f"[[{self.x1}, {self.y1}], [{self.x2}, {self.y2}]]"
 
     def __repr__(self):
-        return self.__str__()
+        return f"{type(self).__name__}({self.x1!r}, {self.y1!r}, {self.x2!r}, {self.y2!r})"
 
     def __and__(self, other):
         return self.intersection(other)
 
+    def __eq__(self, other):
+        return self.rect == other.rect
+
     def reverse_init(self):
-        if not self.x1 <= self.x2:
+        if self.x1 >= self.x2:
             self.x1, self.x2 = self.x2, self.x1
-        if not self.y1 <= self.y2:
+        if self.y1 >= self.y2:
             self.y1, self.y2 = self.y2, self.y1
 
     @staticmethod
-    def xy_cross(self1, self2, other1, other2):
+    def _xy_cross(self1, self2, other1, other2):
+        if self1 > self2 or other1 > other2:
+            raise ValueError(f"Wrong section values passed")
+
         if self2 < other1 or other2 < self1:
             return
         return sorted([self1, self2, other1, other2])[1:-1]
 
     def intersection(self, other):
-        if self.rect == other.rect:
-            return f'Full overlapping', self
+        if self == other:
+            return f"Full overlapping", self
 
-        xs = self.xy_cross(self.x1, self.x2, other.x1, other.x2)
-        ys = self.xy_cross(self.y1, self.y2, other.y1, other.y2)
+        xs = self._xy_cross(self.x1, self.x2, other.x1, other.x2)
+        ys = self._xy_cross(self.y1, self.y2, other.y1, other.y2)
 
         if xs and ys:
             new_rect = Rectangle(xs[0], ys[0], xs[1], ys[1])
-            return 'Intersection', new_rect
+            return "Intersection", new_rect
 
-        return 'No intersection', None
+        return "No intersection", None
