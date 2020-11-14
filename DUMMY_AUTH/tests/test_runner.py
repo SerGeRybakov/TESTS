3f2a22548017500c387349d85ed3c19203e967c4
SerGe <serge.rybakov@gmail.com>
2020-11-14 17:40:38 +0400

    runner tests started

--- DUMMY_AUTH/tests/test_runner.py
+++ DUMMY_AUTH/tests/test_runner.py
@@ -3,11 +3,23 @@ from unittest.mock import patch
 import pytest
 
 from DUMMY_AUTH.auth import runner
+from DUMMY_AUTH.auth.registration import NewUser
 from DUMMY_AUTH.tests.test_auth import Fixtures
 
-class TestFunctions(Fixtures):
-    @pytest.mark.parametrize('new_user', [('John','Smith'),
 
-    ]
-    def test_register(self, existing_user, mock_database,):
-        with patch('builtins.input', )
+# не запускать - меняет боевую базу
+@pytest.mark.skip
+class TestFunctions(Fixtures):
+    @pytest.mark.skip
+    @pytest.mark.parametrize('new_name', [
+        ('John', 'Smith'),
+        ('Valery', 'Pupkin'),
+        ('John', 'Smith'),
+    ])
+    def test_register(self, mock_database, new_name):
+        with patch('builtins.input', side_effect=new_name):
+            assert isinstance(NewUser, runner.register())
+
+
+if __name__ == '__main__':
+    pytest.main()
